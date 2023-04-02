3fce2d6b43fd197a8ed9f05fcc7820477e11f60b
Decostar123 <decostarsharma113@gmail.com>
2023-04-02 10:16:33 +0530

    git ignored many files

---
+++ public/setting.js
@@ -0,0 +1,4 @@
+// const downloadLink = document.getElementById("downloadLink");
+// const wrongPassword = document.getElementById("wrongPassword");
+// downloadLink.style.display = "none";
+// wrongPassword.style.display = "none";
