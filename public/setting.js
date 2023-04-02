d963add4b08ed338a073677a1353c82fde1989f7
Decostar123 <decostarsharma113@gmail.com>
2023-04-02 09:50:54 +0530

    Untrack files in .gitignore

--- public/setting.js
+++
@@ -1,4 +0,0 @@
-// const downloadLink = document.getElementById("downloadLink");
-// const wrongPassword = document.getElementById("wrongPassword");
-// downloadLink.style.display = "none";
-// wrongPassword.style.display = "none";
